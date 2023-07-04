d902369620d7085ddcff5f946d3e68a48c43f84f
maxi <firejama@gmail.com>
2023-07-04 15:33:29 -0400

    Add alert dialogs to confirm deletion from user

--- app/src/main/java/com/blkxltng/to_docompose/ui/screens/task/TaskAppBar.kt
+++ app/src/main/java/com/blkxltng/to_docompose/ui/screens/task/TaskAppBar.kt
@@ -13,10 +13,15 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import com.blkxltng.to_docompose.R
+import com.blkxltng.to_docompose.components.DisplayAlertDialog
 import com.blkxltng.to_docompose.data.models.Priority
 import com.blkxltng.to_docompose.data.models.ToDoTask
 import com.blkxltng.to_docompose.ui.theme.topAppBarContentColor
@@ -111,8 +116,10 @@ fun ExistingTaskAppBar(
             )
         },
         actions = {
-            DeleteAction(onDeleteClicked = navigateToListScreen)
-            UpdateAction(onUpdateClicked = navigateToListScreen)
+            ExistingTaskAppBarActions(
+                selectedTask = selectedTask,
+                navigateToListScreen = navigateToListScreen
+            )
         },
         colors = TopAppBarDefaults.smallTopAppBarColors(
             containerColor = MaterialTheme.colorScheme.primary
@@ -136,11 +143,30 @@ fun CloseAction(
 }
 
 @Composable
+fun ExistingTaskAppBarActions(
+    selectedTask: ToDoTask,
+    navigateToListScreen: (Action) -> Unit
+) {
+    var openDialog by remember {
+        mutableStateOf(false)
+    }
+    DisplayAlertDialog(
+        title = stringResource(id = R.string.alert_dialog_delete_task, selectedTask.title),
+        message = stringResource(id = R.string.alert_dialog_delete_task_confirmation, selectedTask.title),
+        openDialog = openDialog,
+        closeDialog = { openDialog = false },
+        onYesClicked = { navigateToListScreen(Action.DELETE) }
+    )
+    DeleteAction(onDeleteClicked = { openDialog = true })
+    UpdateAction(onUpdateClicked = navigateToListScreen)
+}
+
+@Composable
 fun DeleteAction(
-    onDeleteClicked: (Action) -> Unit
+    onDeleteClicked: () -> Unit
 ) {
     IconButton(onClick = {
-        onDeleteClicked(Action.DELETE)
+        onDeleteClicked()
     }) {
         Icon(
             imageVector = Icons.Filled.Delete,
